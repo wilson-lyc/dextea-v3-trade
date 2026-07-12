a6a2e5d4adaf0c86e63c739de398820f367f6cdb
wilson <wilson_lyc@foxmail.com>
2026-07-12 09:23:46 +0800

    feat: 初始化Spring Boot项目

---
+++ src/test/java/cn/dextea/trade/DexteaTradeApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.dextea.trade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DexteaTradeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
