d1fb22b12660c831bc93f18d8ff034edf4090a2e
wilson <wilson_lyc@foxmail.com>
2026-07-23 12:06:17 +0800

    Generating commit message...

--- src/main/java/cn/dextea/trade/mapper/GalleryMapper.java
+++ src/main/java/cn/dextea/trade/mapper/GalleryMapper.java
@@ -5,6 +5,8 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.List;
+
 @Mapper
 public interface GalleryMapper {
 
