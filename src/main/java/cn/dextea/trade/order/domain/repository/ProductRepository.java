820ad2ef363348a3b240f95dd9c840c4fef5fa88
wilson-lyc <wilson_lyc@foxmail.com>
2026-08-01 16:39:37 +0800

    feat(order): 新增ProductRepository接口
    定义getProductByIds方法，用于根据ID列表批量查询产品聚合根

---
+++ src/main/java/cn/dextea/trade/order/domain/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package cn.dextea.trade.order.domain.repository;
+
+import cn.dextea.trade.order.domain.model.aggregate.Product;
+
+import java.util.List;
+import java.util.Map;
+
+public interface ProductRepository {
+    Map<Long, Product> getProductByIds(List<Long> ids);
+}
