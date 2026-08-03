357288fefcb78e2237ef83afdf9520a635c7c319
wilson <wilson_lyc@foxmail.com>
2026-08-03 16:05:35 +0800

    feat(pay): 新增支付宝配置类
    创建 MyAlipayConfig，基于 AlipayProperties 自动装配支付宝 ApiClient Bean。

--- src/main/java/cn/dextea/trade/pay/infrastructure/config/MyAlipayConfig.java
+++ src/main/java/cn/dextea/trade/pay/infrastructure/config/MyAlipayConfig.java
@@ -1,18 +1,20 @@
 package cn.dextea.trade.pay.infrastructure.config;
 
 import com.alipay.v3.ApiClient;
+import com.alipay.v3.Configuration;
+import com.alipay.v3.util.model.AlipayConfig;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableConfigurationProperties(AlipayProperties.class)
-public class AlipayConfig {
+public class MyAlipayConfig {
 
     @Bean
     public ApiClient alipayApiClient(AlipayProperties properties) {
-        ApiClient apiClient = com.alipay.v3.Configuration.getDefaultApiClient();
-        com.alipay.v3.util.model.AlipayConfig config = new com.alipay.v3.util.model.AlipayConfig();
+        ApiClient apiClient = Configuration.getDefaultApiClient();
+        AlipayConfig config = new AlipayConfig();
         config.setServerUrl(properties.getServerUrl());
         config.setAppId(properties.getAppId());
         config.setPrivateKey(properties.getPrivateKey());
